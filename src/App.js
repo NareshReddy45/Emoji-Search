9409644157c5233acfd86717fa77c7a48d087d70
joshnareddy <joshna.reddy@financepeer.co>
2024-10-05 16:50:55 +0530

    emoji change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import emojiList from './emojiList';
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Handle search input change
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Filter data based on the search term
+  const filteredData = emojiList.filter(item =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <img src="https://cdn.jsdelivr.net/emojione/assets/png/1f638.png" alt="cat-angry"/>
+        <h1>Emoji Search</h1>
+        <img src="https://cdn.jsdelivr.net/emojione/assets/png/1f63a.png" alt="cat-happy"/>
+      </div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <hr/>
+      <ul>
+        {filteredData.map(item => {
+          // Check if item.symbol exists and is not undefined
+          const codePointHex = item.symbol ? item.symbol.codePointAt(0).toString(16) : null;
+          const src = codePointHex ? `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png` : null;
+          return (
+            <div className='card'>
+              <li key={item.id}>
+                <img src={src} alt="emoji" className='emoji'/>
+                <strong className='title-heading'>{item.title}</strong>
+              </li>
+              <span className="copy-placeholder">Click to copy emoji</span>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
